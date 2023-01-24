58d398830c0f528932f4e80d2b7dd953ee0d2b7f
Khadija <khadija@Khadijas-MacBook-Pro.local>
2023-01-24 12:46:19 +0100

    added the side bar component to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,12 @@
 import './home.scss'
+import SideBar from '../../components/sidebar/SideBar'
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className='home'>
+      <SideBar />
+      <div className='homeContainer'>Container</div>
+    </div>
+  )
 }
 
 export default Home
