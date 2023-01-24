cfff09a00b9544ff4824a5e71cb69d70621c0fc1
Khadija <khadija@Khadijas-MacBook-Pro.local>
2023-01-24 15:30:24 +0100

    Styled the navBar component

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,53 @@
 import './navBar.scss'
+import SearchIcon from '@mui/icons-material/Search'
+import LanguageIcon from '@mui/icons-material/Language'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined'
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined'
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined'
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <div className='navBar'>
+      <div className='wrapper'>
+        <div className='search'>
+          <input type='text' placeholder='Search...' />
+          <SearchIcon className='icon' />
+        </div>
+        <div className='items'>
+          <div className='item'>
+            <LanguageIcon className='icon' />
+            English
+          </div>
+          <div className='item'>
+            <DarkModeOutlinedIcon className='icon' />
+          </div>
+          <div className='item'>
+            <FullscreenExitOutlinedIcon className='icon' />
+          </div>
+          <div className='item'>
+            <NotificationsNoneOutlinedIcon className='icon' />
+            <span className='counter'>3</span>
+          </div>
+          <div className='item'>
+            <ChatBubbleOutlineOutlinedIcon className='icon' />
+            <span className='counter'>1</span>
+          </div>
+          <div className='item'>
+            <ListOutlinedIcon className='icon' />
+          </div>
+          <div className='item'>
+            <img
+              src='https://images.pexels.com/photos/15008591/pexels-photo-15008591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=''
+              className='avatar'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NavBar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import './home.scss'
 import SideBar from '../../components/sidebar/SideBar'
+import NavBar from '../../components/navbar/NavBar'
 const Home = () => {
   return (
     <div className='home'>
       <SideBar />
-      <div className='homeContainer'>Container</div>
+      <div className='homeContainer'>
+        <NavBar />
+        Container
+      </div>
     </div>
   )
 }
