d846e2dadb19acd587eda459682e994aa3e38e56
Khadija <khadija@Khadijas-MacBook-Pro.local>
2023-01-24 17:48:38 +0100

    Designed the Widgets component based on the type of the widget

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import './home.scss'
 import SideBar from '../../components/sidebar/SideBar'
 import NavBar from '../../components/navbar/NavBar'
+import Widget from '../../components/widget/Widget'
 const Home = () => {
   return (
     <div className='home'>
       <SideBar />
       <div className='homeContainer'>
         <NavBar />
-        Container
+        <div className='widgets'>
+          <Widget type='user' />
+          <Widget type='order' />
+          <Widget type='earning' />
+          <Widget type='balance' />
+        </div>
       </div>
     </div>
   )
