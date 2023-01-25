d5ef76744256deaeb1771f8f1575062455d59600
Khadija <khadija@Khadijas-MacBook-Pro.local>
2023-01-25 20:52:17 +0100

    Designed the Featured component with React chart libraries and designed the chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import './home.scss'
 import SideBar from '../../components/sidebar/SideBar'
 import NavBar from '../../components/navbar/NavBar'
 import Widget from '../../components/widget/Widget'
+import Chart from '../../components/chart/Chart'
+import Featured from '../../components/featured/Featured'
 const Home = () => {
   return (
     <div className='home'>
@@ -13,6 +15,10 @@ const Home = () => {
           <Widget type='order' />
           <Widget type='earning' />
           <Widget type='balance' />
+        </div>
+        <div className='charts'>
+          <Featured />
+          <Chart title='Last 6 Months (Revenue)' aspect={2 / 1} />
         </div>
       </div>
     </div>
