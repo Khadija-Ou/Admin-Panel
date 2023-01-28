b9f799e47c9082a18813281fda6ff6794aab99c2
Khadija <khadija@Khadijas-MacBook-Pro.local>
2023-01-28 17:34:18 +0100

    Designed the new product and new user pages

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,6 +1,62 @@
 import './new.scss'
-const New = () => {
-  return <div>New</div>
+import Sidebar from '../../components/sidebar/SideBar'
+import Navbar from '../../components/navbar/NavBar'
+import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined'
+import { useState } from 'react'
+
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState('')
+
+  return (
+    <div className='new'>
+      <Sidebar />
+      <div className='newContainer'>
+        <Navbar />
+        <div className='top'>
+          <h1>{title}</h1>
+        </div>
+        <div className='bottom'>
+          <div className='left'>
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+              }
+              alt=''
+            />
+          </div>
+          <div className='right'>
+            <form>
+              <div className='formInput'>
+                <label htmlFor='file'>
+                  Image :{' '}
+                  <DriveFolderUploadOutlinedIcon className='icon' />
+                </label>
+                <input
+                  type='file'
+                  id='file'
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: 'none' }}
+                />
+              </div>
+
+              {inputs.map((input) => (
+                <div className='formInput' key={input.id}>
+                  <label>{input.label}</label>
+                  <input
+                    type={input.type}
+                    placeholder={input.placeholder}
+                  />
+                </div>
+              ))}
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default New
